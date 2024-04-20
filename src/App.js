b4e55e2fb044aa6eb596e37a42d8276729b6c935
James Obogu <155890307+Jamon745@users.noreply.github.com>
2024-04-20 16:56:53 -0700

    Updated Error login page to read Email error page and added password error page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import LoginPage from './Components/LoginPage.jsx';
-import LoginErrorPage from './Components/LoginErrorPage.jsx';
+import EmailErrorPage from './Components/EmailErrorPage.jsx';
+import PasswordErrorPage from './Components/PasswordErrorPage.jsx';
 
 function App() {
   return (
     <div className="App">
       <LoginPage />
-      {/* <LoginErrorPage /> */}
+      {/* <EmailErrorPage /> */}
+      {/* <PasswordErrorPage /> */}
     </div>
   );
 }
