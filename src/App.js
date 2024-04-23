0dd4579c727719db9a3a01e8bbab11d2e5dc504f
James Obogu <155890307+Jamon745@users.noreply.github.com>
2024-04-23 10:09:13 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 
 
-import LoginPage from './Components/Authentication/LoginPage.jsx';
-import EmailErrorPage from './Components/Authentication/EmailErrorPage.jsx';
-import PasswordErrorPage from './Components/Authentication/PasswordErrorPage.jsx';
 
 function App() {
   return (
     <div className="App">
-      <LoginPage />
-      {/* <EmailErrorPage /> */}
-      {/* <PasswordErrorPage /> */}
     </div>
   );
 }
