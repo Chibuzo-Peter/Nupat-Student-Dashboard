a1d77a8a79811f02541867e83f37e2a071685850
povicky1 <povicky1@gmail.com>
2024-04-23 10:17:34 -0700

    working on the sidebar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import React from "react";
+import loginPage from "./Components/Authentication/LoginPage"
+import { Route, Routes } from "react-router-dom";
 
 
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path="/" element={<loginPage />} />
+
+
+
+
+      </Routes>
+
+
     </div>
   );
 }
