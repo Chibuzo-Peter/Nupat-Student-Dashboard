a7dfb94e54064eca63e26bc3a935806d8fa6bf79
James Obogu <155890307+Jamon745@users.noreply.github.com>
2024-04-23 00:23:47 -0700

    Removed some components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import LoginPage from './Components/Authentication/LoginPage.jsx';
-import EmailErrorPage from './Components/Authentication/EmailErrorPage.jsx';
-import PasswordErrorPage from './Components/Authentication/PasswordErrorPage.jsx';
 
 function App() {
   return (
     <div className="App">
       <LoginPage />
-      {/* <EmailErrorPage /> */}
-      {/* <PasswordErrorPage /> */}
     </div>
   );
 }
