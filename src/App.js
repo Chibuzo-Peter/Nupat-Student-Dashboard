844e80fa27630f75cccc178a21750a693fdcfb22
povicky1 <povicky1@gmail.com>
2024-04-23 10:45:01 -0700

    progress

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React from "react";
-import loginPage from "./Components/Authentication/LoginPage"
+
 import { Route, Routes } from "react-router-dom";
+import LoginPage from "./Components/Authentication/LoginPage";
+import Facilitator from "./Pages/Facilitator";
+import Courses from "./Pages/Courses";
+import Profile from "./Pages/Profile";
+import Resources from "./Pages/Resources";
+import Chats from "./Pages/Chats";
+import Calendar from "./Pages/Calendar";
+import Dashboard from "./Pages/Dashboard";
 
 
 
@@ -8,7 +16,14 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<loginPage />} />
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/facilitator" element={<Facilitator />} />
+        <Route path="/courses" element={<Courses />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/resources" element={<Resources />} />
+        <Route path="/chat" element={<Chats />} />
+        <Route path="/calendar" element={<Calendar />} />
 
 
 
