00e7c548b40b0e4b531552ce087a4b03e457cf3b
povicky1 <povicky1@gmail.com>
2024-04-24 12:12:38 -0700

    upo

--- src/Components/DashboardRightbar.jsx
+++ src/Components/DashboardRightbar.jsx
@@ -4,7 +4,12 @@ import  figmaLogo from "../Images/figmaLogo.png"
 import  pinkLine from "../Images/pinkLine.png"
 import miroLogo from "../Images/dataResrcLogo.png"
 import yellowLine from "../Images/yellowLine.png"
-
+import aiLogo from "../Images/aiLogo.png"
+import redLine from "../Images/aiLine.png"
+import psLogo from "../Images/photoshopLogo.png"
+import blueLine from "../Images/blueLine.png"
+import purpleLine from "../Images/purpleLine.png"
+import xdLogo from "../Images/indesignLogo.png"
 
 
 const DashboardRightbar = () => {
@@ -50,14 +55,59 @@ const DashboardRightbar = () => {
         </Box>
         <Box sx={{display:"flex",  marginRight:"80px", alignItem:"left",  marginBlockStart:"30px", padding:"10px", bgcolor:"#FFFFFF",  width:"100%" }}>
             
-            <img src={miroLogo} alt='figmalogo' height="40.53px"  />
+            <img src={aiLogo} alt='figmalogo' height="40.53px"  />
             
             < Container >
-            <Typography variant='h6' component="p" sx={{fontFamily:"inter, sans-serif", display:"flex", flexDirection:"column", fontSize:"15px", fontWeight:"700",}}>Data Resources </Typography>
-            <Typography variant='span' sx={{fontSize:"14px", color:"#B7B6B6", fontFamily:"inter, sans-serif", fontWeight:"600", lineHeight:"16px"}}>Miro</Typography> 
+            <Typography variant='h6' component="p" sx={{fontFamily:"inter, sans-serif", display:"flex", flexDirection:"column", fontSize:"15px", fontWeight:"700",}}>Layout Design </Typography>
+            <Typography variant='span' sx={{fontSize:"14px", color:"#B7B6B6", fontFamily:"inter, sans-serif", fontWeight:"600", lineHeight:"16px"}}>illustrator</Typography> 
             </Container>
             <Container>
-            <img src={yellowLine} alt="test Line" width="150.81px" height="6px"/>
+            <img src={redLine} alt="test Line" width="150.81px" height="6px"/>
+            </Container>    
+            <Typography sx={{fontFamily:"inter, sans-serif", fontWeight:"600", fontSize:"18px"}}>0%</Typography>
+                        
+
+        </Box>
+        <Box sx={{display:"flex",  marginRight:"80px", alignItem:"left",  marginBlockStart:"30px", padding:"10px", bgcolor:"#FFFFFF",  width:"100%" }}>
+            
+            <img src={psLogo} alt='figmalogo' height="40.53px"  />
+            
+            < Container >
+            <Typography variant='h6' component="p" sx={{fontFamily:"inter, sans-serif", display:"flex", flexDirection:"column", fontSize:"15px", fontWeight:"700",}}>Drawing & Editing </Typography>
+            <Typography variant='span' sx={{fontSize:"14px", color:"#B7B6B6", fontFamily:"inter, sans-serif", fontWeight:"600", lineHeight:"16px"}}>Drawing & Editing</Typography> 
+            </Container>
+            <Container>
+            <img src={blueLine} alt="test Line" width="150.81px" height="6px"/>
+            </Container>    
+            <Typography sx={{fontFamily:"inter, sans-serif", fontWeight:"600", fontSize:"18px"}}>0%</Typography>
+                        
+
+        </Box>
+        <Box sx={{display:"flex",  marginRight:"80px", alignItem:"left",  marginBlockStart:"30px", padding:"10px", bgcolor:"#FFFFFF",  width:"100%" }}>
+            
+            <img src={aiLogo} alt='figmalogo' height="40.53px"  />
+            
+            < Container >
+            <Typography variant='h6' component="p" sx={{fontFamily:"inter, sans-serif", display:"flex", flexDirection:"column", fontSize:"15px", fontWeight:"700",}}>Layout Design </Typography>
+            <Typography variant='span' sx={{fontSize:"14px", color:"#B7B6B6", fontFamily:"inter, sans-serif", fontWeight:"600", lineHeight:"16px"}}>illustrator</Typography> 
+            </Container>
+            <Container>
+            <img src={redLine} alt="test Line" width="150.81px" height="6px"/>
+            </Container>    
+            <Typography sx={{fontFamily:"inter, sans-serif", fontWeight:"600", fontSize:"18px"}}>0%</Typography>
+                        
+
+        </Box>
+        <Box sx={{display:"flex",  marginRight:"80px", alignItem:"left",  marginBlockStart:"30px", padding:"10px", bgcolor:"#FFFFFF",  width:"100%" }}>
+            
+            <img src={xdLogo} alt='figmalogo' height="40.53px"  />
+            
+            < Container >
+            <Typography variant='h6' component="p" sx={{fontFamily:"inter, sans-serif", display:"flex", flexDirection:"column", fontSize:"15px", fontWeight:"700",}}>Layout Design </Typography>
+            <Typography variant='span' sx={{fontSize:"14px", color:"#B7B6B6", fontFamily:"inter, sans-serif", fontWeight:"600", lineHeight:"16px"}}>illustrator</Typography> 
+            </Container>
+            <Container>
+            <img src={purpleLine} alt="test Line" width="150.81px" height="6px"/>
             </Container>    
             <Typography sx={{fontFamily:"inter, sans-serif", fontWeight:"600", fontSize:"18px"}}>0%</Typography>
                         
