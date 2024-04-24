7871d97989a01378e4cd494392bbe9ea63d5e821
povicky1 <povicky1@gmail.com>
2024-04-24 11:27:53 -0700

    wow

--- src/Components/DashboardRightbar.jsx
+++ src/Components/DashboardRightbar.jsx
@@ -18,7 +18,7 @@ const DashboardRightbar = () => {
             <Typography sx={{width:"120px", height:"32px",  bgcolor:"#0254A0", borderRadius:"5px", padding:"4px", fontSize:"13px", fontFamily:"inter, sans-serif", color:"#FFFFFF", fontWeight:"600"}}>All Lessons </Typography>
             
         </Box>
-        <Box sx={{display:"flex",  alignItem:"center", marginBlockStart:"30px", padding:"10px", bgcolor:"#FFFFFF",   }}>
+        <Box sx={{display:"flex",  alignItem:"center", marginBlockStart:"30px", padding:"10px", bgcolor:"#FFFFFF",  width:"100%" }}>
             
             <img src={figmaLogo} alt='figmalogo' height="40.53px"  />
             
@@ -33,12 +33,27 @@ const DashboardRightbar = () => {
                         
 
         </Box>
-        <Box sx={{display:"flex",  alignItem:"center", marginBlockStart:"30px", padding:"10px", bgcolor:"#FFFFFF",  width:"100%" }}>
+        <Box sx={{display:"flex",  marginRight:"80px", alignItem:"left",  marginBlockStart:"30px", padding:"10px", bgcolor:"#FFFFFF",  width:"100%" }}>
             
             <img src={miroLogo} alt='figmalogo' height="40.53px"  />
             
-            < Container sx={{flexDirection:"row", lineHeight:"0.5px"}}>
-            <Typography variant='h6' component="p" sx={{fontFamily:"inter, sans-serif", fontSize:"15px", fontWeight:"700",}}>Data Resources </Typography>
+            < Container >
+            <Typography variant='h6' component="p" sx={{fontFamily:"inter, sans-serif", display:"flex", flexDirection:"column", fontSize:"15px", fontWeight:"700",}}>Data Resources </Typography>
+            <Typography variant='span' sx={{fontSize:"14px", color:"#B7B6B6", fontFamily:"inter, sans-serif", fontWeight:"600", lineHeight:"16px"}}>Miro</Typography> 
+            </Container>
+            <Container>
+            <img src={yellowLine} alt="test Line" width="150.81px" height="6px"/>
+            </Container>    
+            <Typography sx={{fontFamily:"inter, sans-serif", fontWeight:"600", fontSize:"18px"}}>0%</Typography>
+                        
+
+        </Box>
+        <Box sx={{display:"flex",  marginRight:"80px", alignItem:"left",  marginBlockStart:"30px", padding:"10px", bgcolor:"#FFFFFF",  width:"100%" }}>
+            
+            <img src={miroLogo} alt='figmalogo' height="40.53px"  />
+            
+            < Container >
+            <Typography variant='h6' component="p" sx={{fontFamily:"inter, sans-serif", display:"flex", flexDirection:"column", fontSize:"15px", fontWeight:"700",}}>Data Resources </Typography>
             <Typography variant='span' sx={{fontSize:"14px", color:"#B7B6B6", fontFamily:"inter, sans-serif", fontWeight:"600", lineHeight:"16px"}}>Miro</Typography> 
             </Container>
             <Container>
