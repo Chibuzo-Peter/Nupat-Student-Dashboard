c7e48b8029d7ca1229c1a01b0c4b246a0d420364
povicky1 <povicky1@gmail.com>
2024-05-06 13:54:25 -0700

    live

--- src/Components/DashboardComponents/DashboardProductInfo.jsx
+++ src/Components/DashboardComponents/DashboardProductInfo.jsx
@@ -8,13 +8,13 @@ const DashboardProductInfo = () => {
       <Box sx={{display:"flex",marginLeft:"8px", justifyContent:"start", marginTop:"6px", flexDirection:"row"}} >
       
       <img src={arrow} style={{width:"15px", }}  alt="" />
-      <span style={{fontSize:"0.6rem", color:"#B5B5B5", fontWeight:"700", marginLeft:"6px"}}> 0 Topics</span>
+      <span style={{fontSize:{xs:"0.6rem", sm:"0.6rem", md:"0.6rem", lg:"1rem"}, color:"#B5B5B5", fontWeight:"700", marginLeft:"6px"}}> 0 Topics</span>
       </Box>
       <Box sx={{display:"flex", marginLeft:"8px", justifyContent:"start", height:"1rem", width:"100%",marginTop:"6px", flexDirection:"row"}} >
       
       <img src={arrow} style={{width:"13px", }}  alt="" />
-      <span style={{fontSize:"0.6rem", color:"#B5B5B5", fontWeight:"700", marginLeft:"7px"}}>0  </span>
-      <span style={{fontSize:"0.6rem", color:"#B5B5B5", fontWeight:"700", marginLeft:"7px"}}> Speakers </span>
+      <span style={{fontSize:{xs:"0.6rem", sm:"0.6rem", md:"0.6rem", lg:"1rem"}, color:"#B5B5B5", fontWeight:"700", marginLeft:"7px"}}>0  </span>
+      <span style={{fontSize:{xs:"0.6rem", sm:"0.6rem", md:"0.6rem", lg:"1rem"}, color:"#B5B5B5", fontWeight:"700", marginLeft:"7px"}}> Speakers </span>
       </Box>
    
     </div>
@@ -22,14 +22,14 @@ const DashboardProductInfo = () => {
     <Box sx={{display:"flex", marginLeft:"4px", justifyContent:"start", height:"1rem", width:"100%",marginTop:"6px", flexDirection:"row"}} >
       
       <img src={arrow} style={{width:"15px", }}  alt="" />
-      <span style={{fontSize:"0.6rem", color:"#B5B5B5", fontWeight:"700", marginLeft:"6px"}}>0  </span>
-      <span style={{fontSize:"0.6rem", color:"#B5B5B5", fontWeight:"700", marginLeft:"6px"}}> Mins </span>
+      <span style={{fontSize:{xs:"0.6rem", sm:"0.6rem", md:"0.6rem", lg:"1rem"}, color:"#B5B5B5", fontWeight:"700", marginLeft:"6px"}}>0  </span>
+      <span style={{fontSize:{xs:"0.6rem", sm:"0.6rem", md:"0.6rem", lg:"1rem"}, color:"#B5B5B5", fontWeight:"700", marginLeft:"6px"}}> Mins </span>
       </Box>
       <Box sx={{display:"flex", marginLeft:"7px", justifyContent:"start", height:"1rem", width:"100%",marginTop:"6px", flexDirection:"row"}} >
       
       <img src={arrow} style={{width:"15px", }}  alt="" />
-      <span style={{fontSize:"0.6rem", color:"#B5B5B5", fontWeight:"700", marginLeft:"6px"}}>0  </span>
-      <span style={{fontSize:"0.6rem", color:"#B5B5B5", fontWeight:"700", marginLeft:"6px"}}> Students </span>
+      <span style={{fontSize:{xs:"0.6rem", sm:"0.6rem", md:"0.6rem", lg:"1rem"}, color:"#B5B5B5", fontWeight:"700", marginLeft:"6px"}}>0  </span>
+      <span style={{fontSize:{xs:"0.6rem", sm:"0.6rem", md:"0.6rem", lg:"1rem"}, color:"#B5B5B5", fontWeight:"700", marginLeft:"6px"}}> Students </span>
       </Box>
    
     </div>
